80841f3cd4d1c660232b45aecd8b1f25ecbd3b40
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-04-26 23:58:50 +0900

    코드 deploy 재테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   return (
     <div>
-      <div>한국어: 안녕 자동 SCP 배포</div>
+      <div>한국어: 안녕 자동 CodeDeploy 배포</div>
       <div>영어: {message}</div>
     </div>
   );
